2c5506aa6a692a95f4372ee7a7d0456df38c3e59
Zerihun Girma <zgirma74@gmail.com>
2023-09-15 20:41:55 +0300

    Create operation.py

---
+++ on-boarding/task-3/operation.py
@@ -0,0 +1,29 @@
+# Function for addition
+def add(x, y):
+    return x + y
+
+# Function for subtraction
+def subtract(x, y):
+    return x - y
+
+# Function for multiplication
+def multiply(x, y):
+    return x * y
+
+# Test addition
+num1 = 10
+num2 = 5
+result_add = add(num1, num2)
+print(f"{num1} + {num2} = {result_add}")
+
+# Test subtraction
+num1 = 10
+num2 = 5
+result_subtract = subtract(num1, num2)
+print(f"{num1} - {num2} = {result_subtract}")
+
+# Test multiplication
+num1 = 10
+num2 = 5
+result_multiply = multiply(num1, num2)
+print(f"{num1} * {num2} = {result_multiply}")
